fb61d447a00e3952e035be1c1d2a8f3c0368b975
George-b1t <george.soares509@gmail.com>
2023-01-08 22:36:25 -0300

    Adicionando verificação de permissões na criação do Estudante

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Request } from "@nestjs/common";
+import { Request as ExpressRequest } from "express";
 import { CreateStudentBody } from "./dtos/create-student-body";
 import { CreateStudentUseCase } from "./use-cases/create-student-use-case";
 
@@ -7,12 +8,16 @@ export class StudentController {
   constructor(private createStudentUseCase: CreateStudentUseCase) {}
 
   @Post("create")
-  async create(@Body() body: CreateStudentBody): Promise<void> {
+  async create(
+    @Body() body: CreateStudentBody,
+    @Request() req: ExpressRequest
+  ): Promise<void> {
     const { name, institution_id } = body;
 
     await this.createStudentUseCase.execute({
       name,
-      institution_id
+      institution_id,
+      by_employee: req["employee"]
     });
   }
 }
--- src/app/modules/student/use-cases/create-student-use-case.ts
+++ src/app/modules/student/use-cases/create-student-use-case.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { Employee } from "@prisma/client";
 import { AppError } from "src/app/errors/app-error";
+import { CheckEmployeePermissionsHelper } from "src/app/helpers/Check-employee-permissions-helper";
 import { InstitutionRepository } from "../../institution/institution-repository";
 import { StudentEntity } from "../student-entity";
 import { StudentRepository } from "../student-repository";
@@ -7,6 +9,7 @@ import { StudentRepository } from "../student-repository";
 interface CreateStudentUseCaseRequest {
   name: string;
   institution_id: string;
+  by_employee: Employee;
 }
 
 @Injectable()
@@ -15,7 +18,21 @@ export class CreateStudentUseCase {
               private institutionRepository: InstitutionRepository) {}
 
   async execute(request: CreateStudentUseCaseRequest): Promise<void> {
-    const { name, institution_id } = request;
+    const { name, institution_id, by_employee } = request;
+
+    // verify if by_employee has permission
+
+    const hasPermission = CheckEmployeePermissionsHelper.execute({
+      permissions: by_employee.permissions,
+      permissionsToCheck: ["create_student"],
+      role: by_employee.role
+    });
+
+    if (!hasPermission) throw new AppError("not allowed to create student")
+
+    // verify if the institution is wrong
+
+    if (by_employee.institution_id !== institution_id) throw new AppError("wrong institution");
 
     // verify if institution exists
 
